262ebc9f65eafb84cd14253cf18daff8aedf49a0
aadii104 <sharmaadi104@gmail.com>
2018-02-18 06:49:28 +0530

    navigator added

--- src/app/outerPages/content-pages/forgot-password/service/forgot-password-page.service.ts
+++ src/app/outerPages/content-pages/forgot-password/service/forgot-password-page.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from '@angular/http';
 // import { UserStateService } from './../../../sharedService/userDetails/user-state.service';
 import { AuthService } from './../../../../shared/auth/auth.service';
 
+import { Router, CanActivate, ActivatedRoute, RouterStateSnapshot } from '@angular/router';
 import { GlobalVaribles } from './../../../../shared/globalVariables/globalVariable';
 
 import 'rxjs/add/operator/map';
@@ -18,7 +19,9 @@ export class PassForgotService {
 
   constructor(private http: Http,
     private _userStateService: AuthService,
-    public _globalVariableService: GlobalVaribles) {
+    public _globalVariableService: GlobalVaribles,
+    private route: ActivatedRoute,
+    private router: Router) {
   }
 
 
@@ -28,6 +31,7 @@ export class PassForgotService {
         this.result = res.json();
         console.log(this.result);
         if (this.result.success) {
+          this.router.navigate(['/newpassword']);
         }
         return user;
       });
