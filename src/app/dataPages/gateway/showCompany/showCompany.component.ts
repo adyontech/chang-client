2630fb2856a3e53358768374b14de1d5f575d426
aadii104 <sharmaadi104@gmail.com>
2018-02-28 11:54:56 +0530

    company list page also done

--- src/app/dataPages/gateway/showCompany/showCompany.component.ts
+++ src/app/dataPages/gateway/showCompany/showCompany.component.ts
@@ -1,8 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { GatewayService } from './../service/gateway.service';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 @Component({
   selector: 'app-show-company',
   templateUrl: './showCompany.component.html',
   styleUrls: ['./showCompany.component.scss'],
 })
-export class ShowCompanyComponent {}
+export class ShowCompanyComponent implements OnInit {
+  companyList: Array<string>;
+  dataCopy: any;
+
+  constructor(public _gatewayService: GatewayService, private router: Router) {}
+
+  ngOnInit() {
+    this.getComanyList();
+  }
+
+  getComanyList() {
+    this.dataCopy = this._gatewayService.getCompanyList().subscribe(data => {
+      this.companyList =  data.json().companyData;
+      console.log(this.companyList)
+      // console.log('hello gateway service')
+    });
+
+    // .map(response => response.json())
+    // .subscribe(data => {
+    //   console.log(data.companyData);
+    //   this.companyList = data.companyData;
+    // });
+  }
+
+  removeCompany(id) {
+    console.log('deleting id : ' + id);
+    this._gatewayService.removeCompany(id).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  viewCompany() {}
+}
