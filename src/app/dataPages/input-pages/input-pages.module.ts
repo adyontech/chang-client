ed3a54d3e0f841cd1722fe6f1a91a030944ec398
aadii104 <sharmaadi104@gmail.com>
2018-02-28 13:11:25 +0530

    input routing model file changed

--- src/app/dataPages/input-pages/input-pages.module.ts
+++ src/app/dataPages/input-pages/input-pages.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { InputPagesRoutingModule } from './input-pages-routing.module';
+import { InputPagesRoutingModule } from './input-routing.module';
 import { ChartistModule} from 'ng-chartist';
 import { AgmCoreModule } from '@agm/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
