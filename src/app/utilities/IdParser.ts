3a9f5d8695bc65d9d531712c8241e7fe93063fd6
Shiven Sinha <shivensinha4@gmail.com>
2018-03-11 12:35:32 +0530

    Fixed for all routes

--- src/app/utilities/IdParser.ts
+++ src/app/utilities/IdParser.ts
@@ -1,6 +1,10 @@
-const regex = /\/form\/(.*)?\//;
+import { routeStructure } from '../shared/routes/input-pages-layout.routes';
+
+const idPosition = routeStructure.split('/').indexOf(':id');
 
 export const ParseId = () => {
-    const path = window.location.pathname;
-    return regex.exec(path)[1];
+    const splitPath = window.location.pathname.split('/');
+    // Some browsers (including chrome) return the path with a leading /, remove it if exists.
+    const sanitizedSplitPath = splitPath[0] === '' ? splitPath.slice(1) : splitPath;
+    return sanitizedSplitPath[idPosition];
 };
