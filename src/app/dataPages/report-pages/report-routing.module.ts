d5a5f33856e4f67724beb59cd6404d96c6035f23
aadii104 <sharmaadi104@gmail.com>
2018-03-19 09:00:06 +0530

    sales report page structures

--- src/app/dataPages/report-pages/report-routing.module.ts
+++ src/app/dataPages/report-pages/report-routing.module.ts
@@ -7,7 +7,7 @@ import { ContraComponent } from './contra/contra.component';
 import { JournalEntryComponent } from './journalEntry/journalEntry.component';
 import { LedgerComponent } from './ledger/ledger.component';
 import { PaymentComponent } from './payment/payment.component';
-
+import { SalesComponent } from "./sales/sales.component";
 const routes: Routes = [
   {
     path: '',
@@ -52,6 +52,13 @@ const routes: Routes = [
         component: PaymentComponent,
         data: {
           title: 'Payment Page',
+        },
+      },
+      {
+        path: 'sales',
+        component: SalesComponent,
+        data: {
+          title: 'Sales Page',
         },
       },
     ],
