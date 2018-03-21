d79bab6973085cf0c7bbc51b804fd554fc795ff5
aadii104 <sharmaadi104@gmail.com>
2018-03-21 14:49:17 +0530

    added pages to _popComponent

---
+++ src/app/dataPages/report-pages/_popComponent/journalEntry/journalEntry.component.ts
@@ -0,0 +1,137 @@
+import { Component, Input, ViewChild, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, FormBuilder, Validators } from '@angular/forms';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import * as alertFunctions from './../../../shared/data/sweet-alerts';
+import { NgbModal, ModalDismissReasons, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ActivatedRoute } from '@angular/router';
+import { JournalEntryService } from './service/journalEntry.service';
+declare var $: any;
+@Component({
+  selector: 'app-journal-entry',
+  templateUrl: './journalEntry.component.html',
+  styleUrls: ['./journalEntry.component.scss'],
+})
+export class JournalEntryComponent implements OnInit {
+  closeResult: string;
+  form: FormGroup;
+  dataCopy: any;
+  paramId: string;
+  totalAmount: number;
+  debitSum: number;
+  creditSum: number;
+  public ledgerList: Array<string> = [];
+  public attachmentError: Boolean = false;
+
+  public value: any = {};
+  public _disabledV: String = '0';
+  public disabled: Boolean = false;
+  constructor(
+    private route: ActivatedRoute,
+    public _journalEntryService: JournalEntryService,
+    public fb: FormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.getRouteParam();
+    this.getLedgerUGNames();
+    this.form = this.fb.group({
+      journalNumber: [''],
+      date: [''],
+      narration: [''],
+      particularsData: this.fb.array([]),
+      file: [''],
+    });
+    this.addParticular();
+  }
+
+  getRouteParam() {
+    this.route.params.subscribe(params => {
+      // console.log(params.id);
+      this.paramId = params.id;
+    });
+  }
+
+  initParticular() {
+    return this.fb.group({
+      particulars: ['', Validators.required],
+      drcr: [''],
+      debitAmount: [''],
+      creditAmount: [''],
+    });
+  }
+
+  addParticular() {
+    // this.totalSum();
+    const control = <FormArray>this.form.controls['particularsData'];
+    const addCtrl = this.initParticular();
+    control.push(addCtrl);
+  }
+
+  removeParticular(i: number) {
+    // this.totalSum();
+    const control = <FormArray>this.form.controls['particularsData'];
+    control.removeAt(i);
+  }
+
+  get formData() {
+    return <FormArray>this.form.get('particularsData');
+  }
+
+  getLedgerUGNames() {
+    this.dataCopy = this._journalEntryService
+      .getLedgerUGNames(this.paramId)
+      .map(response => response.json())
+      .subscribe(data => {
+        this.ledgerList = this.ledgerList.concat(data.ledgerData);
+      });
+  }
+
+  public resetDrCr(value: any, indexValue): void {
+    // const particularsData = <FormArray>this.form.controls['particularsData'];
+    // const array = particularsData.at(indexValue);
+    // array.patchValue({
+    //   debitAmount: '',
+    //   creditAmount: '',
+    // });
+  }
+
+  totalSum() {
+    const formControls = this.form.controls.particularsData['controls'];
+    this.debitSum = 0;
+    this.creditSum = 0;
+    for (let i = 0; i < formControls.length; i++) {
+      const debitAmount = formControls[i].controls.debitAmount.value;
+      const creditAmount = formControls[i].controls.creditAmount.value;
+
+      if (!isNaN(debitAmount) && debitAmount !== '') {
+        this.debitSum += parseFloat(debitAmount);
+      }
+      if (!isNaN(creditAmount) && creditAmount !== '') {
+        this.creditSum += parseFloat(creditAmount);
+      }
+    }
+  }
+  onFileChange(event) {
+    this.attachmentError = false;
+    console.log(event.target.files[0].size);
+    const reader = new FileReader();
+
+    if (event.target.files[0].size < 400000) {
+      if (event.target.files && event.target.files.length > 0) {
+        this.form.get('file').setValue(event.target.files[0]);
+      }
+    } else {
+      this.attachmentError = true;
+    }
+  }
+
+  onSubmit(user) {
+    alertFunctions.SaveData().then(datsa => {
+      if (datsa) {
+        console.log(user);
+        this._journalEntryService.createNewEntry(user, this.paramId).subscribe(data => {});
+      }
+    });
+  }
+}
---
+++ src/app/dataPages/report-pages/_popComponent/sales/service/sales.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+
+import { GlobalVaribles } from './../../../../shared/globalVariables/globalVariable';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/';
+@Injectable()
+export class SalesService {
+  private paramCompanyName: string;
+  result: {};
+  token: string;
+  windowStorage: any;
+  _url: string;
+
+  constructor(
+    private http: Http,
+    private router: Router,
+    private route: ActivatedRoute,
+    public _globalVariableService: GlobalVaribles
+  ) {
+    this.windowStorage = JSON.parse(window.localStorage.getItem('user'));
+    this.token = this.windowStorage.token;
+  }
+  createNewEntry(user: any, compName) {
+    const form = new FormData();
+    for (const key of Object.keys(user)) {
+      // console.log(key , user[key])
+      form.append(key, user[key]);
+    }
+    this._url = `${this._globalVariableService.baseServerUrl}/api/sales?token=${this.token}&companyName=${
+      compName
+    }`;
+    return this.http.post(this._url, user).map((res: Response) => {
+      this.result = res.json();
+      // console.log(this.result)
+    });
+  }
+  getLedgerUGNames(compName) {
+    this._url = `${this._globalVariableService.baseServerUrl}/api/ledgerNameList?token=${this.token}&&companyName=${
+      compName
+    }`;
+    return this.http.get(this._url);
+  }
+  getSalesUGNames(compName) {
+    this._url = `${this._globalVariableService.baseServerUrl}/api/salesLedgerList?token=${this.token}&&companyName=${
+      compName
+    }`;
+    return this.http.get(this._url);
+  }
+
+  getprsrList(compName) {
+    this._url = `${this._globalVariableService.baseServerUrl}/api/prsrList?token=${this.token}&&companyName=${
+      compName
+    }`;
+    return this.http.get(this._url);
+  }
+}
