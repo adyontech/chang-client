2812d7d60d49cef3223821ebc4a4a6a89b25a0cf
aadii104 <sharmaadi104@gmail.com>
2018-04-01 12:01:53 +0530

    me component done.

--- src/app/accountSettings/me/me.component.ts
+++ src/app/accountSettings/me/me.component.ts
@@ -9,14 +9,19 @@ import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from
 export class MeProfileComponent implements OnInit {
   user: any;
   bio;
+  gender;
   accountType;
   email;
   phoneNo;
+  occupation;
   profileUpdated;
   readCollabId;
   writeCollabId;
   userName;
   plan;
+  updatedAt;
+  joinedOn;
+  website;
   constructor(public _meService: MeService, private router: Router) {}
 
   ngOnInit() {
@@ -28,12 +33,17 @@ export class MeProfileComponent implements OnInit {
       console.log(this.user);
       this.bio = this.user.bio;
       this.plan = this.user.accountType.plan;
+      this.updatedAt = this.user.accountType.updatedAt;
       this.email = this.user.email;
+      this.website = this.user.website;
+      this.occupation = this.user.occupation;
       this.phoneNo = this.user.phoneNo;
       this.profileUpdated = this.user.profileUpdated;
       this.readCollabId = this.user.readCollabId;
       this.writeCollabId = this.user.writeCollabId;
       this.userName = this.user.userName;
+      this.joinedOn = this.user.accountType.createdAt
+      this.gender = this.user.gender;
     });
   }
 }
