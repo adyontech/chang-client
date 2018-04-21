e454357ad150363c6e3b3330e445b41af6049bea
aadii104 <sharmaadi104@gmail.com>
2018-04-21 10:56:21 +0530

    basic mailer settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,18 @@ import { Component, ViewContainerRef } from '@angular/core';
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
-    // Set toastr container ref configuration for toastr positioning on screen
-    constructor(public toastr: ToastsManager, vRef: ViewContainerRef) {
-        this.toastr.setRootViewContainerRef(vRef);
-    }
+  // Set toastr container ref configuration for toastr positioning on screen
+  constructor(public toastr: ToastsManager, vRef: ViewContainerRef) {
+    this.toastr.setRootViewContainerRef(vRef);
+    // console.log(
+    //   '%c%s',
+    //   'color: green; background: cyan; font-size: 32px;',
+    //   'Well aadii104 is trying to make things better here, let him know him you are looking for something ( try not to start somehthing your own)'
+    // );
+  }
 }
