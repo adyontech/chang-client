0bab4b82de0cb9e7ef0e205f4cc16dc396b9ab0f
aadii104 <sharmaadi104@gmail.com>
2018-05-19 20:51:37 +0530

    styling sidebar

--- src/app/shared/routes/input-pages-layout.routes.ts
+++ src/app/shared/routes/input-pages-layout.routes.ts
@@ -1,17 +1,20 @@
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from "@angular/router";
 
 // Route for content layout without sidebar, navbar and footer for pages like Login, Registration etc...
 
 export const DATA_PAGES_ROUTES: Routes = [
   {
-    path: ':owner/:id/form',
-    // loadChildren: './dataPages/gateway/gateway-pages.module#GatewayPagesModule',
-    loadChildren: './dataPages/input-pages/input-pages.module#InputPagesModule',
+    path: ":owner/:id/form",
+    loadChildren: "./dataPages/input-pages/input-pages.module#InputPagesModule"
   },
   {
-    path: ':owner/:id/report',
-    // loadChildren: './dataPages/gateway/gateway-pages.module#GatewayPagesModule',
-    loadChildren: './dataPages/report-pages/report-pages.module#ReportPagesModule',
+    path: ":owner/:id/report",
+    loadChildren:
+      "./dataPages/report-pages/report-pages.module#ReportPagesModule"
   },
+  {
+    path: ":owner/:id/task",
+    loadChildren: "./dataPages/taskboard/taskboard.module#TaskboardModule"
+  }
 ];
 export const routeStructure: string = DATA_PAGES_ROUTES[0].path;
