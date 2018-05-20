4b1cd216fe2c18711a1128e435593a28157376dc
aadii104 <sharmaadi104@gmail.com>
2018-05-20 07:43:25 +0530

    pulling the taskboard to services file

--- src/app/dataPages/taskboard/taskboard.model.ts
+++ src/app/dataPages/taskboard/taskboard.model.ts
@@ -1,18 +1,24 @@
 export class Task {
-    public taskTitle: string;
-    public taskMessage: string;
-    public createdOn: string;
-    public createdBy: string;
-    public assignedTo: string;
-    public status: string;
-  
-    constructor(taskTitle: string, taskMessage:string, createdOn: string, createdBy: string, assignedTo: string, status: string) {
-      this.taskTitle = taskTitle;
-      this.taskMessage = taskMessage;
-      this.createdOn = createdOn;
-      this.createdBy = createdBy;
-      this.assignedTo = assignedTo;
-      this.status = status;
-    }
+  public taskTitle: string;
+  public taskMessage: string;
+  public createdOn: number;
+  public createdBy: string;
+  public assignedTo: string;
+  public status: string;
+
+  constructor(
+    taskTitle: string,
+    taskMessage: string,
+    createdOn: number,
+    createdBy: string,
+    assignedTo: string,
+    status: string
+  ) {
+    this.taskTitle = taskTitle;
+    this.taskMessage = taskMessage;
+    this.createdOn = createdOn;
+    this.createdBy = createdBy;
+    this.assignedTo = assignedTo;
+    this.status = status;
   }
-  
+}
