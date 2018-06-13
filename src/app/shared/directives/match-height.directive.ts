ac0583923ae5766bc5a7e544cb42bcf10eb2d437
aadii104 <sharmaadi104@gmail.com>
2018-06-13 20:29:43 +0530

    making matching patcher.

--- src/app/shared/directives/match-height.directive.ts
+++ src/app/shared/directives/match-height.directive.ts
@@ -1,63 +1,68 @@
 import {
-    Directive, ElementRef, AfterViewInit,
-    Input, HostListener, NgModule
+  Directive,
+  ElementRef,
+  AfterViewInit,
+  Input,
+  HostListener,
+  NgModule,
 } from '@angular/core';
 
 @Directive({
-    selector: '[matchHeight]'
+  selector: '[matchHeight]',
 })
 export class MatchHeightDirective implements AfterViewInit {
-    // class name to match height
-    @Input()
-    matchHeight : string;
+  // class name to match height
+  @Input() matchHeight: string;
 
-    constructor(private el: ElementRef) {
-    }
+  constructor(private el: ElementRef) {}
 
-    ngAfterViewInit() {
-        // call our matchHeight function here
-        setTimeout(() => { this.matchHeights(this.el.nativeElement, this.matchHeight )}, 300);
-    }
+  ngAfterViewInit() {
+    // call our matchHeight function here
+    setTimeout(() => {
+      this.matchHeights(this.el.nativeElement, this.matchHeight);
+    }, 300);
+  }
 
-    matchHeights(parent: HTMLElement, className: string) {
-        // match height logic here
+  matchHeights(parent: HTMLElement, className: string) {
+    // match height logic here
 
-        if (!parent) return;
+    if (!parent) return;
 
-        // step 1: find all the child elements with the selected class name
-        const children = parent.getElementsByClassName(className);
+    // step 1: find all the child elements with the selected class name
+    const children = parent.getElementsByClassName(className);
 
-        if (!children) return;
+    if (!children) return;
 
-        //Pixinvent - Match hight - fix --- comment below code
-        Array.from(children).forEach((x: HTMLElement) => {
-            x.style.height = 'initial';
-        });
+    //Pixinvent - Match hight - fix --- comment below code
+    Array.from(children).forEach((x: HTMLElement) => {
+      x.style.height = 'initial';
+    });
 
-        // step 2a: get all the child elements heights
-        const itemHeights = Array.from(children)
-            .map(x => x.getBoundingClientRect().height);
+    // step 2a: get all the child elements heights
+    const itemHeights = Array.from(children).map(
+      x => x.getBoundingClientRect().height
+    );
 
-        // step 2b: find out the tallest
-        const maxHeight = itemHeights.reduce((prev, curr) => {
-            return curr > prev ? curr : prev;
-        }, 0);
+    // step 2b: find out the tallest
+    const maxHeight = itemHeights.reduce((prev, curr) => {
+      return curr > prev ? curr : prev;
+    }, 0);
 
-        // step 3: update all the child elements to the tallest height
-        Array.from(children)
-            .forEach((x: HTMLElement) => x.style.height = `${maxHeight}px`);
-    }
+    // step 3: update all the child elements to the tallest height
+    Array.from(children).forEach(
+      (x: HTMLElement) => (x.style.height = `${maxHeight}px`)
+    );
+  }
 
-    @HostListener('window:resize')
-    onResize() {
-        // call our matchHeight function here
-        this.matchHeights(this.el.nativeElement, this.matchHeight );
-    }
+  @HostListener('window:resize')
+  onResize() {
+    // call our matchHeight function here
+    this.matchHeights(this.el.nativeElement, this.matchHeight);
+  }
 }
 
 @NgModule({
-    declarations: [MatchHeightDirective],
-    exports: [MatchHeightDirective]
+  declarations: [MatchHeightDirective],
+  exports: [MatchHeightDirective],
 })
-
-export class MatchHeightModule { }
+export class MatchHeightModule {}
