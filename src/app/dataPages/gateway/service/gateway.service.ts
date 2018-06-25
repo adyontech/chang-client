fa9d4e913336b642f9e23dcb9385286eac9df1d1
aadii104 <sharmaadi104@gmail.com>
2018-06-25 19:06:30 +0530

    added reset epoch date function.

--- src/app/dataPages/gateway/service/gateway.service.ts
+++ src/app/dataPages/gateway/service/gateway.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@angular/core';
 // import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { Http, Response, RequestOptions, Headers } from '@angular/http';
 import { GlobalVaribles } from './../../../shared/globalVariables/globalVariable';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {
+  Router,
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { Routes, RouterModule, ActivatedRoute } from '@angular/router';
 
 import 'rxjs/add/operator/map';
@@ -33,23 +38,30 @@ export class GatewayService {
     }
   }
   createNewCompany(user: any) {
+    console.log(user);
     const form = new FormData();
     for (const key of Object.keys(user)) {
       form.append(key, user[key]);
     }
-    this._url = `${this._globalVariableService.baseServerUrl}/api/gateway?token=${this.token}`;
+    this._url = `${
+      this._globalVariableService.baseServerUrl
+    }/api/gateway?token=${this.token}`;
     return this.http.post(this._url, form).map((res: Response) => {
       this.result = res.json();
-      return this.result;
+      return (this.result = res.json());
     });
   }
 
   getCompanyList() {
-    this._url = `${this._globalVariableService.baseServerUrl}/api/gatewaylist?token=${this.token}`;
+    this._url = `${
+      this._globalVariableService.baseServerUrl
+    }/api/gatewaylist?token=${this.token}`;
     return this.http.get(this._url);
   }
 
   removeCompany(id) {
-    return this.http.delete(this._url + '/' + id).map((res: Response) => res.json());
+    return this.http
+      .delete(this._url + '/' + id)
+      .map((res: Response) => res.json());
   }
 }
