37f8184018470dc2acfe7fcefcb0cdb7254ce041
aadii104 <sharmaadi104@gmail.com>
2018-07-25 22:02:17 +0530

    payment report done.

--- src/app/dataPages/report-pages/payment/payment.component.ts
+++ src/app/dataPages/report-pages/payment/payment.component.ts
@@ -6,6 +6,8 @@ import {
 } from '@ng-bootstrap/ng-bootstrap';
 import { ActivatedRoute } from '@angular/router';
 import { PaymentService } from './service/payment.service';
+import { GlobalCompanyService } from './../../../shared/globalServices/oneCallvariables.servce';
+
 declare var $: any;
 
 @Component({
@@ -17,8 +19,17 @@ export class PaymentComponent implements OnInit {
   // Models
   closeResult: string;
   editContentId: String = '';
-  public dateFrom: Date;
-  public dateTo: Date;
+
+  public companyStartingDate;
+  public companyEndingDate;
+  public choosenStartDate;
+  public csd;
+  public choosenEndDate;
+  public ced;
+  public showStartDateError: Boolean = false;
+  public showEndDateError: Boolean = false;
+  public minNgbDate;
+  public maxNgbDate;
 
   // Modal for column hide and show
 
@@ -34,7 +45,7 @@ export class PaymentComponent implements OnInit {
 
   public dataCopy: any;
   public paramId: string;
-  public ownerId: string;
+  public ownerName: string;
 
   public accountTypeModel = 'All';
   public chooseItem = [
@@ -46,18 +57,107 @@ export class PaymentComponent implements OnInit {
 
   public chooseItemBox = [];
   public accountType: Array<string> = ['All', 'Cash', 'Bank'];
-  public incomingData: Array<string> = [];
+  public mainIncomingData = [];
+  public incomingData = [];
 
   constructor(
     private route: ActivatedRoute,
     private modalService: NgbModal,
-    public _paymentService: PaymentService
+    public _paymentService: PaymentService,
+    public _globalCompanyService: GlobalCompanyService
   ) {}
   ngOnInit() {
     this.getRouteParam();
     this.onAccSelect('All');
+    this.getGlobalCompanyData();
   }
 
+  getGlobalCompanyData() {
+    this.dataCopy = this._globalCompanyService
+      .getGlobalCompanyData(this.paramId, this.ownerName)
+      .map(response => response.json())
+      .subscribe(data => {
+        this.companyStartingDate = data.startDate;
+        this.companyEndingDate = data.endDate;
+        const minD = new Date(parseInt(data.startDate, 0));
+        this.minNgbDate = {
+          year: minD.getFullYear(),
+          month: minD.getMonth() + 1,
+          day: minD.getDate(),
+        };
+        const maxD = new Date(parseInt(data.endDate, 0));
+        this.maxNgbDate = {
+          year: maxD.getFullYear(),
+          month: maxD.getMonth() + 1,
+          day: maxD.getDate(),
+        };
+        this.dateFilterRefresh();
+      });
+  }
+  startDate(value) {
+    console.log(value);
+    const dateReturn = this.dateRangeValidator(value);
+    if (dateReturn.allow) {
+      this.showStartDateError = false;
+      this.choosenStartDate = dateReturn.date;
+      this.setDateFilter();
+    } else {
+      this.showStartDateError = true;
+      this.choosenStartDate = this.companyStartingDate;
+    }
+  }
+
+  endDate(value) {
+    const dateReturn = this.dateRangeValidator(value);
+    if (dateReturn.allow) {
+      this.showEndDateError = false;
+      this.choosenEndDate = dateReturn.date;
+      this.setDateFilter();
+    } else {
+      this.showEndDateError = true;
+      this.choosenEndDate = this.companyEndingDate;
+    }
+  }
+
+  dateRangeValidator(arg) {
+    if (arg !== null && typeof arg === 'object') {
+      const dateVal = new Date(arg.year, arg.month - 1, arg.day).getTime();
+
+      if (
+        dateVal > 0 &&
+        dateVal >= this.companyStartingDate &&
+        dateVal <= this.companyEndingDate
+      ) {
+        return { allow: true, date: dateVal };
+      } else {
+        this.incomingData = [];
+        return { allow: false, date: null };
+      }
+    } else {
+      this.incomingData = [];
+
+      return { allow: false, date: null };
+    }
+  }
+  setDateFilter() {
+    console.log(this.mainIncomingData);
+    this.incomingData = this.mainIncomingData.filter(el => {
+      if (
+        el.date >= this.choosenStartDate &&
+        el.date <= this.choosenEndDate &&
+        el.date >= this.companyStartingDate &&
+        el.date <= this.companyEndingDate
+      ) {
+        return el;
+      }
+    });
+    console.log(this.incomingData);
+  }
+
+  dateFilterRefresh() {
+    this.choosenStartDate = this.companyStartingDate;
+    this.choosenEndDate = this.companyEndingDate;
+  }
   // This function is used in open
   private getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
@@ -73,7 +173,7 @@ export class PaymentComponent implements OnInit {
     this.route.params.subscribe(params => {
       // console.log(params.id);
       this.paramId = params.id;
-      this.ownerId = params.owner;
+      this.ownerName = params.owner;
     });
   }
 
@@ -158,7 +258,7 @@ export class PaymentComponent implements OnInit {
   // real date picker active from here
   getIncomingData(selectionValue) {
     this.dataCopy = this._paymentService
-      .getIncomingData(selectionValue, this.paramId, this.ownerId)
+      .getIncomingData(selectionValue, this.paramId, this.ownerName)
       .map(response => response.json())
       .subscribe(data => {
         console.log(data);
@@ -169,7 +269,7 @@ export class PaymentComponent implements OnInit {
 
   getAllIncomingData() {
     this.dataCopy = this._paymentService
-      .getAllIncomingData(this.paramId, this.ownerId)
+      .getAllIncomingData(this.paramId, this.ownerName)
       .map(response => response.json())
       .subscribe(data => {
         console.log(data);
@@ -180,7 +280,7 @@ export class PaymentComponent implements OnInit {
   deleteEntry(id) {
     console.log(id);
     this._paymentService
-      .deleteEntry(id, this.paramId, this.ownerId)
+      .deleteEntry(id, this.paramId, this.ownerName)
       .map(response => response.json())
       .subscribe(data => {
         console.log(data);
