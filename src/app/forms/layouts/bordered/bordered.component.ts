67d06f7117a52346d097775aa84de4326d10cae7
aadii104 <sharmaadi104@gmail.com>
2018-08-06 21:54:53 +0530

    removed extra files and code.

--- src/app/forms/layouts/bordered/bordered.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-bordered',
-  templateUrl: './bordered.component.html',
-  styleUrls: ['./bordered.component.scss']
-})
-export class BorderedComponent {
-}
